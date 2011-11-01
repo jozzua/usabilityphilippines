c44e5fcb4245dc5413ee29d46f2dced5fa32b85e
Ely Apao <jozzua@gmail.com>
2011-11-02 07:51:41 +0800

    first commit

---
+++ Gemfile
@@ -0,0 +1,18 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.9'
+gem 'sqlite3-ruby','1.2.5', :require => 'sqlite3'
+gem 'haml'
+gem 'compass'
+gem 'jquery-rails', '>= 1.0.12'
+
+group :development do
+  gem 'rspec-rails', '2.6.1'
+  gem 'sass'
+end
+
+group :test do
+  gem 'rspec-rails', '2.6.1'
+  gem 'spork', '0.9.0.rc8'
+end
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Usabilityphilippines::Application.config.secret_token = '4d15c4dfb38fc9054dd459860741241165891aaacecea64bf5f6c9387e1eef43c9c6568eceeb6485eefb5c77871c4415324666746ea706d88c3543f4e33b0268'
